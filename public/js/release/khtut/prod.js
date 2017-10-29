e185ab8f19843cd2f16b6a2e24887415dc70a02a
Ramsey Nasser <ram@nas.sr>
2017-10-28 21:25:31 -0400

    Add public folder

---
+++ public/js/release/khtut/prod.js
@@ -0,0 +1,27 @@
+// Compiled by ClojureScript 1.9.908 {:static-fns true, :optimize-constants true}
+goog.provide('khtut.prod');
+goog.require('cljs.core');
+goog.require('cljs.core.constants');
+goog.require('khtut.core');
+cljs.core._STAR_print_fn_STAR_ = (function() { 
+var G__13817__delegate = function (_){
+return null;
+};
+var G__13817 = function (var_args){
+var _ = null;
+if (arguments.length > 0) {
+var G__13818__i = 0, G__13818__a = new Array(arguments.length -  0);
+while (G__13818__i < G__13818__a.length) {G__13818__a[G__13818__i] = arguments[G__13818__i + 0]; ++G__13818__i;}
+  _ = new cljs.core.IndexedSeq(G__13818__a,0,null);
+} 
+return G__13817__delegate.call(this,_);};
+G__13817.cljs$lang$maxFixedArity = 0;
+G__13817.cljs$lang$applyTo = (function (arglist__13819){
+var _ = cljs.core.seq(arglist__13819);
+return G__13817__delegate(_);
+});
+G__13817.cljs$core$IFn$_invoke$arity$variadic = G__13817__delegate;
+return G__13817;
+})()
+;
+khtut.core.init_BANG_();
